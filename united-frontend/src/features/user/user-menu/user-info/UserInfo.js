07308e350e17705c18dc858e5d141ff617788ff3
Tony LE <pro.tony.le@outlook.com>
2021-12-15 23:36:37 +0100

    update design v7

--- united-frontend/src/features/user/user-menu/user-info/UserInfo.js
+++ united-frontend/src/features/user/user-menu/user-info/UserInfo.js
@@ -3,31 +3,34 @@ import "./UserInfo.css";
 import {Card} from "primereact/card";
 import {Divider} from "primereact/divider";
 import {Panel} from "primereact/panel";
+import {ScrollPanel} from "primereact/scrollpanel";
 
 function UserInfo() {
     return <div className="user-contents">
         <Card title="Vos informations personnelles" subTitle="Vous pouvez sur cette page modifier l'ensemble des données vous concernant" style={{ height: '100%' }}>
             <Divider/>
-            <Panel header="Informations basiques">
-                <p><span>Prénom : </span>Chaeyoung is bae</p>
-                <Divider />
 
-                <p><span>Nom de famille : </span>Park</p>
-                <Divider />
+            <ScrollPanel style={{width: '100%', height: '70vh'}}>
+                <Panel header="Informations basiques">
+                    <p><span>Prénom : </span>Chaeyoung is bae</p>
+                    <Divider />
 
-                <p><span>Date de naissance : </span>11/02/1997</p>
-                <Divider />
+                    <p><span>Nom de famille : </span>Park</p>
+                    <Divider />
 
-                <p><span>Genre :  </span>Femme</p>
-            </Panel>
+                    <p><span>Date de naissance : </span>11/02/1997</p>
+                    <Divider />
 
-            <Panel header="Informations de contact">
-                <p><span>Adresse mail :  </span>rosie@blackpink.kr</p>
-                <Divider />
+                    <p><span>Genre :  </span>Femme</p>
+                </Panel>
 
-                <p><span>Numéro de téléphone :  </span> +823532450845</p>
-            </Panel>
+                <Panel header="Informations de contact">
+                    <p><span>Adresse mail :  </span>rosie@blackpink.kr</p>
+                    <Divider />
 
+                    <p><span>Numéro de téléphone :  </span> +823532450845</p>
+                </Panel>
+            </ScrollPanel>
         </Card>
     </div>
 }
--- united-frontend/src/features/user/user-menu/user-security/UserSecurity.js
+++ united-frontend/src/features/user/user-menu/user-security/UserSecurity.js
@@ -3,26 +3,31 @@ import "./UserSecurity.css";
 import {Card} from "primereact/card";
 import {Divider} from "primereact/divider";
 import {Panel} from "primereact/panel";
+import {ScrollPanel} from "primereact/scrollpanel";
 
 function UserSecurity() {
     return <div className="user-security">
         <Card title="Sécurité & Paiement" subTitle="Vous pouvez sur cette page modifier l'ensemble des données vous concernant" style={{ height: '100%' }}>
             <Divider/>
-            <Panel header="Informations de connexion">
-                <p><span>Adresse email</span> : rosie@blackpink.kr</p>
-                <Divider/>
 
-                <p><span>Mot de passe : </span> ********</p>
-            </Panel>
-            <Panel header="Informations de paiement">
-                        <p><span>Type de paiement :  </span> Carte bancaire</p>
-                        <Divider />
+            <ScrollPanel style={{width: '100%', height: '70vh'}}>
+                <Panel header="Informations de connexion">
+                    <p><span>Adresse email</span> : rosie@blackpink.kr</p>
+                    <Divider/>
 
-                        <p><span>BIC :  </span> CERGERF</p>
-                        <Divider />
+                    <p><span>Mot de passe : </span> ********</p>
+                </Panel>
+                <Panel header="Informations de paiement">
+                    <p><span>Type de paiement :  </span> Carte bancaire</p>
+                    <Divider />
 
-                        <p><span>IBAN : </span> FR04 5454 4541 5645 441</p>
-            </Panel>
+                    <p><span>BIC :  </span> CERGERF</p>
+                    <Divider />
+
+                    <p><span>IBAN : </span> FR04 5454 4541 5645 441</p>
+                </Panel>
+            </ScrollPanel>
+
         </Card>
     </div>
 }
