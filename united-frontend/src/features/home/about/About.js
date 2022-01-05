f5ca477d97e2fa7d75758a8116c17314c1d42ff6
wilfrid beaunes <kesnelbeaunes@gmail.com>
2022-01-05 23:26:56 +0100

    correction bug page vitrine

--- united-frontend/src/features/home/about/About.js
+++ united-frontend/src/features/home/about/About.js
@@ -11,19 +11,19 @@ function About() {
 
         <div className="p-d-flex p-jc-evenly services-card">
             <div className="p-mr-4">
-                <img className="image-apropo" src={us} /> 
-                <h2 className="space-text description-text">Qui sommes-nous ?</h2>
-                <h4 className="space-text description-text"> Nous sommes une équipe de développeur informatique localisé à Belfort et nous mettons notre savoir au service des associations. </h4>
+                <img className="image-apropo" src={us} alt='us'/> 
+                <h2>Qui sommes-nous ?</h2>
+                <h4 className="space-text "> Nous sommes une équipe de développeur informatique localisé à Belfort et nous mettons notre savoir au service des associations. </h4>
             </div>
             <div className="p-mr-4">
-                <img className="image-apropo" src={defis} /> 
-                <h2 className="space-text description-text">Nos défis</h2>
-                <h4 className="space-text description-text">Mettre en relation des associations avec des donnateurs, facilité les donnations, mettre à la disposition des donnateurs et associations des éléments permettant de traiter l'information (outils graphiques, factures...) </h4>
+                <img className="image-apropo" src={defis} alt='defis'/> 
+                <h2>Nos défis</h2>
+                <h4 className="space-text ">Mettre en relation des associations avec des donnateurs, facilité les donnations, mettre à la disposition des donnateurs et associations des éléments permettant de traiter l'information (outils graphiques, factures...) </h4>
             </div>
             <div>
-                <img className="image-apropo" src={sharing} />
-                <h2 className="space-text description-text"> Notre vision</h2>
-                <h4 className="space-text description-text">Nous avons une vision d'entraide, solidarité, ensemble, unité, amour, fraternité, bonté, coopération, charité, et de partage. Ainsi, nous pensons que chacun peur apporter sa contribution et deviennir le bâtisseur d'un monde fondé</h4>
+                <img className="image-apropo" src={sharing} alt='sharing'/>
+                <h2> Notre vision</h2>
+                <h4 className="space-text ">Nous avons une vision d'entraide, solidarité, ensemble, unité, amour, fraternité, bonté, coopération, charité, et de partage. Ainsi, nous pensons que chacun peur apporter sa contribution et deviennir le bâtisseur d'un monde fondé</h4>
             </div>
         </div> 
         
--- united-frontend/src/features/home/services/Services.js
+++ united-frontend/src/features/home/services/Services.js
@@ -8,22 +8,21 @@ import all_assoc from "../../../images/images_vitrine/all_assoc.jpg"
 function Services() {
     return <div className="services" id="services">
         <h2>Nos services</h2>
-        
         <div className="p-d-flex p-jc-evenly services-card">
             <div className="p-mr-4">
-                <img className="image-services" src={toward} /> 
-                <h2 className="space-text description-text">Mettez en avant votre association</h2>
-                <h4 className="space-text description-text"> Nous mettons votre association en avant en lui donnant de la visibilité. United.co un moyen dereferencier votre association. Nos équipes collaboront avec vous pour la création de contre compte association et vous auriez le libre choix de proposer vos services. </h4>
+                <img className="image-services" src={toward} alt="image-toward"/>    
+                <h2>Mettez en avant votre association</h2>
+                <h4 className="space-text"> Nous mettons votre association en avant en lui donnant de la visibilité. United.co un moyen dereferencier votre association. Nos équipes collaboront avec vous pour la création de contre compte association et vous auriez le libre choix de proposer vos services. </h4> 
             </div>
             <div className="p-mr-4">
-                <img className="image-services" src={statistic} /> 
-                <h2 className="space-text description-text">Simplifier la gestion de vos adhésions</h2>
-                <h4 className="space-text description-text">Pour vous association, nous proposons des outils graphiques permettant d'analyser rapidement par exemple les fonds reçus, le nombre d'adhérant... vous être maître des informations qui apparaissent sur United.co. </h4>
+                <img className="image-services" src={statistic} alt="image-statistic"/> 
+                <h2>Simplifier la gestion de vos adhésions</h2>
+                <h4 className="space-text">Pour vous association, nous proposons des outils graphiques permettant d'analyser rapidement par exemple les fonds reçus, le nombre d'adhérant... vous être maître des informations qui apparaissent sur United.co. </h4>
             </div>
-            <div>   
-                <img className="image-services" src={all_assoc} />
-                <h2 className="space-text description-text">Vos associations au même endroit</h2>
-                <h4 className="space-text description-text">Pour vous donateurs, nous mettons à votre disposition une liste d'associations auxquelles vous pouvez faire des dons en quelque second, vous pourriez choisir vos associations favorites,  vous avez les détails de vos factures (facture annuelle et mensuelle) et la résiliation d'un abonnement ce fait en quelque click.</h4>
+            <div className="p-mr-4">   
+                <img className="image-services " src={all_assoc} alt="image-all_assoc"/>    
+                <h2>Vos associations au même endroit</h2>
+                <h4 className="space-text">Pour vous donateurs, nous mettons à votre disposition une liste d'associations auxquelles vous pouvez faire des dons en quelques secondes, vous pourriez choisir vos associations favorites,  vous avez les détails de vos factures (facture annuelle et mensuelle) et la résiliation d'un abonnement ce fait en quelque click.</h4>  
             </div>
         </div> 
     </div>
