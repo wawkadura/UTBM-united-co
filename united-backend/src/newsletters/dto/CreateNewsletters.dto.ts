9df749737838df4206d959b75cb8703f63a868a9
wilfrid beaunes <kesnelbeaunes@gmail.com>
2022-01-12 19:44:39 +0100

    page accueil

--- united-backend/src/newsletters/dto/CreateNewsletters.dto.ts
+++
@@ -1,4 +0,0 @@
-export class CreateNewslettersDTO {
-    // id: number;
-    email: string;
-  }
--- united-backend/src/newsletters/newsletters.controller.ts
+++ united-backend/src/newsletters/newsletters.controller.ts
@@ -1,25 +1,28 @@
-import { Controller, Body, Post,HttpStatus} from '@nestjs/common';
-import { CreateNewslettersDTO } from './dto/CreateNewsletters.dto';
+import { Controller, Body, Post,HttpStatus ,Get} from '@nestjs/common';
 import { NewslettersDTO } from './dto/Newsletters.dto';
 import { NewslettersService } from './newsletters.service';
 
-@Controller('newsletters')
+@Controller('newsletters') //route name 
 export class NewslettersController {
     constructor(private readonly newslettersService: NewslettersService){}
 
-    // @Post()
-    // public async createOne(@Body() createNewletter: CreateNewslettersDTO){
-    //     const resp =await this.newslettersService.createOne(createNewletter)
-    //     return resp;
-    // }
-
     @Post()
     async createUsers(@Body() data: NewslettersDTO) {
-       const newsl = await this.newslettersService.create(data);
-      return {
-        statusCode: HttpStatus.OK,
-        message: 'User created successfully',
-        newsl
-      };
+      const em =  await this.newslettersService.read(data.email); // check if the email value don't exit
+      if (!em){
+        const newsL = await this.newslettersService.create(data); //save the new email  if it doesn"t exist
+        return {
+          statusCode: HttpStatus.OK,
+          message: 'Email enregistré avec succès.',
+          newsL
+        };
+      }
+      else {   //if email exist i send a mesasge towards front
+        return {
+          statusCode: 201,
+          message: 'L\'email existe déja.',
+          data,
+        };
+      }
     }
 }
