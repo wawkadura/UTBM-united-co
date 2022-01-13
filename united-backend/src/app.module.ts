aca6fc8030eaadcaa8507d48da9b36473af93562
wilfrid beaunes <kesnelbeaunes@gmail.com>
2022-01-13 16:49:01 +0100

    change auto migration and add newsletters in features

--- united-backend/src/app.module.ts
+++ united-backend/src/app.module.ts
@@ -3,7 +3,7 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './features/user/user.module';
 import { DatabaseModule } from './database/database.module';
-import { NewslettersModule } from './newsletters/newsletters.module';
+import { NewslettersModule } from './features/newsletters/newsletters.module';
 
 @Module({
   imports: [UserModule, DatabaseModule, NewslettersModule],
