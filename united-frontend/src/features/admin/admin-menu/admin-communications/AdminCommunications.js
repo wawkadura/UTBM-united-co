e3f39a68bfefdb2f1b0c166a8a674c37fa49b4ec
elwalid kadura <elwalid.kadura@visiperf.io>
2022-01-24 21:57:46 +0000

    feat(admin/front): completing the admin communications

--- united-frontend/src/features/admin/admin-menu/admin-communications/AdminCommunications.js
+++ united-frontend/src/features/admin/admin-menu/admin-communications/AdminCommunications.js
@@ -1,54 +1,45 @@
-import emailjs from '@emailjs/browser';
 import React, { useState, useRef } from 'react';
-import { Editor } from 'primereact/editor';
 import { Card } from "primereact/card";
 import { InputText } from "primereact/inputtext";
 import { Toast } from 'primereact/toast';
 import { Button } from "primereact/button";
 import { AdminService } from '../../AdminService';
+import { classNames } from 'primereact/utils';
 import { InputTextarea } from "primereact/inputtextarea";
 import "./AdminCommunications.css"
-
 import { useForm, Controller } from 'react-hook-form';
 
 function AdminCommunications() {
-    const { control, formState: { errors }, handleSubmit, reset } = useForm({ defaultValues });
-
-    var emailData = {
-        email: '',
-        subject: '',
-        message: ''
-    }
-
-    var defaultValues = {
-        email: '',
-        subject: '',
-        message: ''
-    }
+    const [formData, setFormData] = useState({});
     const toast = useRef(null);
+    const [isPending, setIsPending] = useState(false);
     const adminService = new AdminService()
+    const defaultValues = {
+        subject: "",
+        email : "",
+        message: ""
+    }
+    const { control, formState: { errors }, handleSubmit, reset } = useForm({defaultValues});
 
-    const updateSubject = (event) => {
-        emailData.subject = event.target.value
-    }
-    const updateEmail = (event) => {
-        emailData.email = event.target.value
-    }
-    const updateMessage = (event) => {
-        emailData.message = event.target.value
-        console.log(emailData)
-    }
-    function sendEmail() {
-        emailData.message = "<html>" + emailData.message + "</html>"
-        adminService.sendEmail(emailData).then((response) => {
+    function sendEmail(data) {
+        setFormData(data);
+        setIsPending(true)
+        console.log(formData)
+        adminService.sendEmail(formData).then((response) => {
             console.log(response);
             toast.current.show({ severity: 'success', summary: 'Confirmation', detail: 'Le mail a bien été envoyé !', life: 3000 });
+            setIsPending(false)
+            reset()
         }, (error) => {
             console.log(error.text);
+            setIsPending(false)
             // toast.current.show({ severity: 'error', summary: 'Erreur', detail: response.status + ": " + response.statusText, life: 3000 });
         });
-    }
 
+    }
+    const getFormErrorMessage = (name) => {
+        return errors[name] && <small className="p-error">{errors[name].message}</small>
+    };
 
     return (
         <div className='communcations-contener'>
@@ -56,31 +47,49 @@ function AdminCommunications() {
 
 
             <Card title="Communications" subTitle="Vous pouvez envoyer un mail à n'importe quelle utilisateur" style={{ height: '100%' }}>
-                <div className="p-fluid p-formgrid p-grid">
-                    <div className="p-field p-col-6 p-md-6">
-                        <label htmlFor="email">À</label>
-                        <span className="p-input-icon-left">
-                            <i className="pi pi-at" />
-                            <InputText name="email" onInput={(event) => updateEmail(event)} type="text" />
-                        </span>
-                    </div>
-                    <div className="p-field p-col-6 p-md-6">
-                        <label htmlFor="nom">Sujet</label>
-                        <span className="p-input-icon-left">
-                            <InputText name="subject" onInput={(event) => updateSubject(event)} type="text" />
-                        </span>
+                <form onSubmit={handleSubmit(sendEmail)} className="p-fluid">
+                    <div className="p-fluid p-formgrid p-grid">
+                        <div className="p-field p-col-6 p-md-6">
+                            <label htmlFor="email">À</label>
+                            <span className="p-input-icon-left">
+                                <Controller name="email" control={control}
+                                    rules={{ required: 'Email obligatoire.', pattern: { value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i, message: 'Adresse email invalide. Ex: example@email.com' } }}
+                                    render={({ field, fieldState }) => (
+                                        <InputText id={field.name} placeholder="Email" {...field} className={classNames({ 'p-invalid': fieldState.invalid })} />
+                                    )} />
+                            </span>
+                            {getFormErrorMessage('email')}
+                        </div>
+                        <div className="p-field p-col-6 p-md-6">
+                            <label htmlFor="nom">Sujet</label>
+                            <span className="p-input-icon-left">
+                                <Controller name="subject" control={control} rules={{ required: 'sujet obligatoire.' }} render={({ field, fieldState }) => (
+                                    <InputText id={field.name} placeholder="Sujet" {...field} className={classNames({ 'p-invalid': fieldState.invalid })} />
+                                )} />
+                            </span>
+                            {getFormErrorMessage('subject')}
+                        </div>
+
+                        <div className="p-field p-col-12 p-md-12">
+                            <label htmlFor="nom">Message</label>
+                            <span className="p-input-icon-left">
+                                <Controller name="message" control={control} rules={{ required: 'sujet obligatoire.' }} render={({ field, fieldState }) => (
+                                    <InputTextarea placeholder="Message" id={field.name} rows={8} {...field} className={classNames({ 'p-invalid': fieldState.invalid })} />
+                                )} />
+                            </span>
+                            {getFormErrorMessage('message')}
+                        </div>
                     </div>
 
-                    <div className="p-field p-col-12 p-md-12">
-                        <label htmlFor="nom">Message</label>
-                        <span className="p-input-icon-left">
-                            <InputTextarea rows={8} name="message" onInput={(event) => updateMessage(event)} />
-                        </span>
-                    </div>
-                </div>
+                    <br />
+                    {isPending ?
+                        <Button className="mail-button" disabled type="submit" label="Envoi..." icon="pi pi-envelope" />
+                        :
+                        <Button className="mail-button" type="submit" label="Envoyer" icon="pi pi-envelope" />
 
-                <br />
-                <Button className="mail-button" label="Envoyer" icon="pi pi-envelope" onClick={sendEmail} />
+                    }
+
+                </form>
             </Card>
         </div>
     );
