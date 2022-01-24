640ba3f1ceadf0730fe948a13d27bad1a838c73d
elwalid kadura <elwalid.kadura@visiperf.io>
2022-01-24 21:59:38 +0000

    feat(admin/front): adding the statistics with graphs

--- united-frontend/src/features/admin/admin-menu/admin-statistics/AdminStatistics.js
+++ united-frontend/src/features/admin/admin-menu/admin-statistics/AdminStatistics.js
@@ -1,6 +1,86 @@
+import "./AdminStatistics.css";
+import React from 'react';
+import { Card } from "primereact/card";
+import { TabView, TabPanel } from 'primereact/tabview';
+import GraphUsers from "./graphs/graph-users/GraphUsers";
+import GraphDonations from "./graphs/graph-donations/GraphDonations";
+import GraphBugs from "./graphs/graph-bugs/GraphBugs";
+import { useState, useRef, useEffect } from "react";
+import { Toast } from 'primereact/toast';
+import { AdminService } from "../../AdminService";
+import { ProgressSpinner } from 'primereact/progressspinner';
 
 function AdminStatistics() {
-    return <div className="admin-statistics-contener">oooooooouuuuuuuuuuuu stats</div>
+    const adminService = new AdminService()
+    const [isPending, setIsPending] = useState(true);
+    const toast = useRef(null);
+
+    var dataUsers = {
+        associations: {
+            2022: [12, 51, 62, 33, 21, 62, 45, 12, 51, 62, 33, 50],
+            2021: [65, 59, 80, 81, 56, 55, 40, 40, 65, 59, 65, 59],
+            2020: [12, 51, 62, 33, 21, 62, 45, 12, 51, 62, 33, 50],
+
+        },
+        donors: {
+            2022: [65, 59, 80, 81, 56, 55, 40, 40, 65, 59, 65, 59],
+            2021: [12, 51, 62, 33, 21, 62, 45, 12, 51, 62, 33, 50],
+            2020: [65, 59, 80, 81, 56, 55, 40, 40, 65, 59, 65, 59],
+        },
+    }
+
+    var dataDonations = {
+        donations: {
+            2022: [65, 59, 80, 81, 56, 55, 40, 40, 65, 59, 65, 59],
+            2021: [12, 51, 62, 33, 21, 62, 45, 12, 51, 62, 33, 50],
+            2020: [65, 59, 80, 81, 56, 55, 40, 40, 65, 59, 65, 59],
+        },
+    }
+
+    var dataBugs = {
+        bugs: {
+            2022: [65, 59, 80, 81, 56, 55, 40, 40, 65, 59, 65, 59],
+            2021: [12, 51, 62, 33, 21, 62, 45, 12, 51, 62, 33, 50],
+            2020: [65, 59, 80, 81, 56, 55, 40, 40, 65, 59, 65, 59],
+        },
+    }
+    useEffect(() => {
+        adminService.getAdminStats().then((response) => {
+            // console.log(response)
+            if (!response.ok && toast.current != null) {
+                //toast.current.show({ severity: 'error', summary: 'Erreur', detail: response.statusCode + ": " + response.message, life: 3000 });
+            }
+            if (response.ok && toast.current != null) {
+                setStats(response.data)
+                //toast.current.show({ severity: 'success', summary: 'Confirmation', detail: 'Les associations ont bien été récupérés', life: 3000 });
+            }
+            setIsPending(false)
+        });
+    });
+    const setStats = (jsonResponse) => {
+        console.log(jsonResponse)
+     }
+
+    return (
+        <Card title="Statistiques générales" style={{ height: '100%' }}>
+            <Toast ref={toast} />
+            <div className="card">
+                {isPending ? <div className="spinner"> <ProgressSpinner /></div> :
+                    <TabView className="tabview-header-icon">
+                        <TabPanel header="Donations" leftIcon="pi pi-heart">
+                            <GraphDonations data={dataDonations} />
+                        </TabPanel>
+                        <TabPanel header="Utilisateurs" leftIcon="pi pi-user">
+                            <GraphUsers data={dataUsers} />
+                        </TabPanel>
+                        <TabPanel header="Bugs" leftIcon="pi pi-server">
+                            <GraphBugs data={dataBugs} />
+                        </TabPanel>
+                    </TabView>
+                }
+            </div>
+        </Card>
+    )
 }
 
 export default AdminStatistics
---
+++ united-frontend/src/features/admin/admin-menu/admin-statistics/graphs/graph-bugs/GraphBugs.js
@@ -0,0 +1,83 @@
+import "./GraphBugs.css";
+import React from 'react';
+import { Chart } from 'primereact/chart';
+import { useState, useEffect } from "react";
+import { Dropdown } from 'primereact/dropdown';
+
+
+function GraphBugs(bugs) {
+    var data = bugs.data
+    const [labels] = useState(['Janvier', 'Février', 'Mars', 'Avril', 'Mai', 'Juin', 'Juillet', 'Août', 'Septembre', 'Octobre', 'Novembre', 'Décembre'])
+    const [bugsStats, setBugsStats] = useState([])
+    const [selectedYear, setSelectedYear] = useState();
+    const [years, setYears] = useState([])
+    const lineStylesData = {
+        labels: labels,
+        datasets: [
+            {
+                label: 'bugs',
+                data: bugsStats,
+                fill: true,
+                borderColor: 'rgba(355,100,81)',
+                tension: .4,
+                backgroundColor: 'rgba(355,100,81,0.2)'
+            }
+        ]
+    };
+    let basicOptions = {
+        maintainAspectRatio: false,
+        aspectRatio: .6,
+        plugins: {
+            legend: {
+                labels: {
+                    color: '#495057'
+                }
+            }
+        },
+        scales: {
+            x: {
+                ticks: {
+                    color: '#495057'
+                },
+                grid: {
+                    color: '#ebedef'
+                }
+            },
+            y: {
+                ticks: {
+                    color: '#495057'
+                },
+                grid: {
+                    color: '#ebedef'
+                }
+            }
+        }
+    };
+    const onYearChange = (e) => {
+        setSelectedYear(e.value);
+        changeDataYear(e.value)
+    }
+    useEffect(() => {
+        var keys = Object.keys(data.bugs)
+        setYears(keys)
+        setSelectedYear(keys[0]);
+        changeDataYear(keys[0])
+    }, []);
+    const changeDataYear = (year) => {
+        Object.keys(data.bugs).map(function (key) {
+            if (key === year) {
+                setBugsStats(data.bugs[key])
+            }
+        });
+    }
+    return (
+        <div className="graph-bugs-card">
+            <h5>Statistiques des nouveaux bugs signalés par les utilisateurs de la platforme</h5>
+            <br />
+            <Dropdown value={selectedYear} options={years} onChange={onYearChange} />
+            <Chart type="line" data={lineStylesData} options={basicOptions} />
+        </div>
+    )
+}
+
+export default GraphBugs
