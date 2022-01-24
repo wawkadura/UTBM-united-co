c1b16ff72a9d0910e176d05e4e86d61d5c677d6e
ssan93 <san.steven97@gmail.com>
2022-01-24 22:07:49 +0100

    feat(ticket): [Back] add back API

--- united-backend/src/app.module.ts
+++ united-backend/src/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from './app.service';
 import { UserModule } from './features/user/user.module';
 import { DatabaseModule } from './database/database.module';
 import { NewslettersModule } from './features/newsletters/newsletters.module';
+import { TicketModule } from './features/ticket/ticket.module';
 
 @Module({
-  imports: [UserModule, DatabaseModule, NewslettersModule],
+  imports: [UserModule, DatabaseModule, NewslettersModule, TicketModule],
   controllers: [AppController],
   providers: [AppService],
 })
