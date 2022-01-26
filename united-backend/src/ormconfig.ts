ba9b0b6c8f69a2255b1cd0005969f7feaf8ae89d
elwalid kadura <elwalid.kadura@visiperf.io>
2022-01-26 14:09:04 +0000

    fix(back/config): change to localhost

--- united-backend/src/ormconfig.ts
+++ united-backend/src/ormconfig.ts
@@ -12,7 +12,7 @@ import { ConnectionOptions } from 'typeorm';
 // Check typeORM documentation for more information.
 const config: ConnectionOptions = {
     type: 'mysql',
-    host: 'host.docker.internal',
+    host: 'localhost',
     port: 3306,
     username: 'root',
     password: '',
